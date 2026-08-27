a73abe501404f210d4e9523609855e013fd1f2e4
ahbe0911-cmd <ahbe0911@gmail.com>
2026-08-27 09:58:47 +0330

    Bump DasteYar compact UI version

--- dasteyar-build/app/build.gradle.kts
+++ dasteyar-build/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ir.dasteyar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
     }
 
     buildTypes {
