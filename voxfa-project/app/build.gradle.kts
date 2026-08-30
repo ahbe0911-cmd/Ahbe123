00cd812d33aa0e2f44f56b46708236a0ed501e19
ahbe0911-cmd <ahbe0911@gmail.com>
2026-08-30 14:10:42 +0330

    VoxFa: bump version to 1.2 vivid

--- voxfa-project/app/build.gradle.kts
+++ voxfa-project/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ir.voxfa.keyboard"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0-smart"
+        versionCode = 3
+        versionName = "1.2.0-vivid"
     }
 
     buildTypes {
